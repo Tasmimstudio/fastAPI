836656ce162542a121dd696b5880ee03ac7cf6c5
Ashuraclashers <ashuraclashers@gmail.com>
2025-10-02 08:15:09 +0800

    newcom

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ from neomodel import config
 # Load variables from .env
 load_dotenv()
 
-NEO4J_URI = os.getenv("NEO4J_URI")
-NEO4J_USER = os.getenv("NEO4J_USER")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_URI = os.getenv("neo4j+s://cd3d134b.databases.neo4j.io")
+NEO4J_USER = os.getenv("neo4j")
+NEO4J_PASSWORD = os.getenv("hXIk1yeZPYikOlfvyKpIUcvX7HoEYPadIOdw7T05MjI")
 
 if not NEO4J_URI or not NEO4J_USER or not NEO4J_PASSWORD:
     raise ValueError("❌ Missing Neo4j environment variables (NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD)")
